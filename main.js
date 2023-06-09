c17d944c081b08863f8fa5eca6a1449d14104a35
HPalways07 <130910380+HPalways07@users.noreply.github.com>
2023-06-09 17:24:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+  document.getElementById("toppart").style.width = "30vw";
+  document.getElementById("m").style.width = "70vw"
+}
+
+function closeNav() {
+  document.getElementById("toppart").style.width = "0";
+  document.getElementById("m").style.width = "100vw"
+}
